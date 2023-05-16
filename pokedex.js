9ef07f7a0d33bf0ad2631af87026368ecc0fec3d
taliapipa <125282306+taliapipa@users.noreply.github.com>
2023-05-16 15:16:32 +0200

    busqueda realizada

--- pokedex.js
+++ pokedex.js
@@ -1,79 +1,77 @@
-
 // Obtener referencias a los elementos HTML
 const listaPokedex = document.getElementById("pokedex");
+const searchInput = document.getElementById("searchInput");
 
 // Variable que almacenará los datos de la API
 let allPokemon = [];
 
 // Función para obtener los datos de la API
 const getApi = async () => {
-const url = `https://pokeapi.co/api/v2/pokemon/?offset=20&limit=150`;
-const resp = await fetch(url);
-const respJson = await resp.json();
+  const url = `https://pokeapi.co/api/v2/pokemon/?offset=20&limit=150`;
+  const resp = await fetch(url);
+  const respJson = await resp.json();
 
   // Guardar los datos en la variable allPokemon
-allPokemon = [...respJson.results];
+  allPokemon = [...respJson.results];
 };
 
 // Función para renderizar los datos en la pantalla
-const renderPokemon = () => {
-  // Mapear los datos de la API para crear un array de objetos con los datos de cada Pokemon
-const pokemon = allPokemon.map((result) => ({
-    name: result.name,
-    image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${result.url.split('/')[6]}.png`,
-    type: "",
-    id: result.url.split('/')[6]
-}));
+const renderPokemon = (pokemonData) => {
+  // Limpiar la lista de Pokemon previos
+  listaPokedex.innerHTML = "";
 
   // Bucle for of para crear los elementos HTML para cada Pokemon y agregarlos a la lista
-for (const pokemonData of pokemon) {
+  for (const pokemon of pokemonData) {
     // Crear un elemento li para cada Pokemon
     const li = document.createElement("li");
 
     // Crear el contenido HTML para el elemento li con los datos del Pokemon
     li.innerHTML = `
-    <img src="${pokemonData.image}" alt="">
-    <h2>${pokemonData.name}</h2>
-    <h3>${pokemonData.type}</h3>
-    <p>${pokemonData.id}</p>
+      <img src="${pokemon.image}" alt="">
+      <h2>${pokemon.name}</h2>
+      <h3>${pokemon.type}</h3>
+      <p>${pokemon.id}</p>
     `;
 
     // Agregar el elemento li a la lista
     listaPokedex.appendChild(li);
-}
+  }
 };
 
-//Crear el div 
+// Función para filtrar los Pokemon según la búsqueda
+const filterPokemon = (query) => {
+  const filteredResults = allPokemon.filter(item => item.name.toLowerCase().includes(query));
 
-const div = document.createElement("div");
-div.innerHTML = `
-<input type="text" id="searchInput" placeholder="Buscar...">
-<ul id="resultsList"></ul> `;
+  // Mapear los datos filtrados para obtener los detalles de cada Pokemon
+  const pokemonData = filteredResults.map((result) => ({
+    name: result.name,
+    image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${result.url.split('/')[6]}.png`,
+    type: "",
+    id: result.url.split('/')[6]
+  }));
 
-//traer el input a JS
+  // Renderizar los Pokemon filtrados en la pantalla
+  renderPokemon(pokemonData);
+};
 
-const searchInput = document.querySelector("#searchInput");
-
-//Crear el event
-input.addEventListener("input", handleInputSearch);
-
-//funcion event
-
-function handleInputSearch(ev){
-  const pokeSearch = ev.target.value;
-  
-}
-
-
+// Evento para manejar la entrada en el campo de búsqueda
+searchInput.addEventListener("input", (ev) => {
+  const query = ev.target.value.toLowerCase();
+  filterPokemon(query);
+});
 
 // Función principal para iniciar el programa
 const init = async () => {
   // Obtener los datos de la API
-await getApi();
+  await getApi();
 
-  // Renderizar los datos en la pantalla
-renderPokemon();
+  // Renderizar todos los Pokemon en la pantalla inicialmente
+  filterPokemon("");
+
+  // Enfocar el campo de búsqueda al cargar la página
+  searchInput.focus();
 };
 
 // Llamar a la función init() para iniciar el programa
 init();
+
